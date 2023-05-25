4a4473d886535b08f36a324229560ae657e2d682
Evgeny Polyakov <evg_pol@yahoo.com>
2023-05-25 19:51:00 +0300

    fixed styles

--- src/App.js
+++ src/App.js
@@ -1,78 +1,78 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-const audioClips = [
+const clips = [
   {
     keyCode: 81,
-    keyTrigger: 'Q',
-    id: 'Heater 1',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    keyText: 'Q',
+    clipDesc: 'Heater 1',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
   },
   {
     keyCode: 87,
-    keyTrigger: 'W',
-    id: 'Heater 2',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    keyText: 'W',
+    clipDesc: 'Heater 2',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
   },
   {
     keyCode: 69,
-    keyTrigger: 'E',
-    id: 'Heater 3',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    keyText: 'E',
+    clipDesc: 'Heater 3',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
   },
   {
     keyCode: 65,
-    keyTrigger: 'A',
-    id: 'Heater 4',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    keyText: 'A',
+    clipDesc: 'Heater 4',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
   },
   {
     keyCode: 83,
-    keyTrigger: 'S',
-    id: 'Clap',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    keyText: 'S',
+    clipDesc: 'Clap',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
   },
   {
     keyCode: 68,
-    keyTrigger: 'D',
-    id: 'Open-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    keyText: 'D',
+    clipDesc: 'Open-HH',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
   },
   {
     keyCode: 90,
-    keyTrigger: 'Z',
-    id: "Kick-n'-Hat",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    keyText: 'Z',
+    clipDesc: "Kick-n'-Hat",
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
   },
   {
     keyCode: 88,
-    keyTrigger: 'X',
-    id: 'Kick',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    keyText: 'X',
+    clipDesc: 'Kick',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
   },
   {
     keyCode: 67,
-    keyTrigger: 'C',
-    id: 'Closed-HH',
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    keyText: 'C',
+    clipDesc: 'Closed-HH',
+    clipURL: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
   }
 ];
 
-function Pad({ clip, volume, setRecording }) {
-  const [active, setActive] = useState(false);
+function Pad({ clip, volume, setClipName }) {
+  const [activeKey, setActiveKey] = useState(false);
 
   const playSound = () => {
-    const audioTag = document.getElementById(clip.keyTrigger);
-    audioTag.volume = volume;
-    audioTag.currentTime = 0;
+    const sound = document.getElementById(clip.keyText);
+    sound.volume = volume;
+    sound.currentTime = 0;
 
-    var playPromise = audioTag.play();
+    const playPromise = sound.play();
     if (playPromise !== undefined) playPromise.catch(function () { });
 
-    setRecording(clip.id);
+    setClipName(clip.clipDesc);
 
-    setActive(true);
-    setTimeout(() => setActive(false), 200);
+    setActiveKey(true);
+    setTimeout(() => setActiveKey(false), 200);
   }
 
   const handleKeyPress = (e) => {
@@ -90,30 +90,30 @@ function Pad({ clip, volume, setRecording }) {
   }, []);
 
   return (
-    <button onClick={playSound} className={`drum-pad ${active && 'test'}`} id={clip.id}>
-      <audio className='clip' id={clip.keyTrigger} src={clip.url} />
-      {clip.keyTrigger}
+    <button onClick={playSound} className={`drum-pad ${activeKey && 'active'}`} id={clip.clipDesc}>
+      <audio className='clip' id={clip.keyText} src={clip.clipURL} />
+      {clip.keyText}
     </button>
   );
 }
 
 function App() {
   const [volume, setVolume] = useState(0.3);
-  const [recording, setRecording] = useState('');
+  const [clipName, setClipName] = useState('');
 
   return (
     <div className="App">
       <div id='drum-machine'>
-        <div className='pad'>
-          {audioClips.map((clip) => (
-            <Pad key={clip.id} clip={clip} volume={volume} setRecording={setRecording} />
+        <div className='drumPad'>
+          {clips.map((clip) => (
+            <Pad key={clip.keyText} clip={clip} volume={volume} setClipName={setClipName} />
           ))}
         </div>
 
         <div id='display'>
-          <input type='range' step='0.01' value={volume} max='1' min='0' onChange={(e) => setVolume(e.target.value)} />
+          <div className='display'>{clipName}</div>
 
-          <div>{recording}</div>
+          <input type='range' className='volume' step='0.01' value={volume} max='1' min='0' onChange={(e) => setVolume(e.target.value)} />
         </div>
       </div>
     </div>
